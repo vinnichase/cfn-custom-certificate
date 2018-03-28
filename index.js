d42799c0b317876a268606a728f67813dc337e15
vincenzkoop <vincenz.koop@digitalunit.berlin>
2018-03-28 16:03:34 +0200

    build lambda deployment package

--- index.js
+++
@@ -1,3 +0,0 @@
-exports.createCertificate = require('./src/create-certificate');
-exports.observeCertificate = require('./src/observe-certificate');
-exports.deleteCertificate = require('./src/acm/delete-certificate');
--- src/create-certificate.js
+++ src/create-certificate.js
@@ -1,6 +1,7 @@
-var requestCertificate = require('./acm/request-certificate')
-var describeCertificate = require('./acm/describe-certificate')
-var validateCertificate = require('./route53/validate-certificate')
+var requestCertificate = require('./acm/request-certificate');
+var describeCertificate = require('./acm/describe-certificate');
+var deleteCertificate = require('./acm/delete-certificate');
+var validateCertificate = require('./route53/validate-certificate');
 
 function createCertificate(domainName, validationMethod, hostedZoneId) {
     return requestCertificate(domainName, validationMethod)
@@ -16,4 +17,22 @@ function createCertificate(domainName, validationMethod, hostedZoneId) {
         });
 }
 
-module.exports = createCertificate;
+exports.handler = function (event, context) {
+    if (event.RequestType == "Delete") {
+        var certificateArn = event.PhysicalResourceId;
+        deleteCertificate(certificateArn).then(function (data) {
+            response.send(event, context, response.SUCCESS, data, event.PhysicalResourceId);
+        }).catch(function (err) {
+            response.send(event, context, response.FAILED, { Error: err.message }, event.PhysicalResourceId);
+        });
+    } else if (event.RequestType == "Create") {
+        var domainName = event.ResourceProperties.DomainName;
+        var validationMethod = event.ResourceProperties.ValidationMethod;
+        var hostedZoneId = event.ResourceProperties.HostedZoneId;
+        createCertificate(domainName, validationMethod, hostedZoneId).then(function (data) {
+            response.send(event, context, response.SUCCESS, data, event.PhysicalResourceId);
+        }).catch(function (err) {
+            response.send(event, context, response.FAILED, { Error: err.message });
+        });
+    }
+};
